0cf4b2cf0e96d52fc549be33e3e3cdf7875ba721
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-11-08 20:36:35 -0300

    added tests fixes

--- internal/order/usecase/calculate_price_test.go
+++ internal/order/usecase/calculate_price_test.go
@@ -2,17 +2,18 @@ package usecase
 
 import (
 	"database/sql"
+	"testing"
+
 	"github.com/Cerebrovinny/products_monitor/internal/order/entity"
 	"github.com/Cerebrovinny/products_monitor/internal/order/infra/database"
 	"github.com/stretchr/testify/suite"
-	"testing"
-	//sqlite3
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type CalculatePriceUseCaseTestSuite struct {
 	suite.Suite
-	OrderRepository *database.OrderRepository
+	OrderRepository database.OrderRepository
 	Db              *sql.DB
 }
 
@@ -20,9 +21,10 @@ func (suite *CalculatePriceUseCaseTestSuite) SetupTest() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	suite.NoError(err)
 
-	_, err = db.Exec("CREATE TABLE orders (id TEXT, price REAL, tax REAL, final_price REAL)")
+	// create table orders
+	_, err = db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
 	suite.Db = db
-	suite.OrderRepository = database.NewOrderRepository(db)
+	suite.OrderRepository = *database.NewOrderRepository(db)
 }
 
 func (suite *CalculatePriceUseCaseTestSuite) TearDownTest() {
@@ -33,7 +35,7 @@ func TestSuite(t *testing.T) {
 	suite.Run(t, new(CalculatePriceUseCaseTestSuite))
 }
 
-func (suite *CalculatePriceUseCaseTestSuite) TestCalculatePriceFinalPrice() {
+func (suite *CalculatePriceUseCaseTestSuite) TestCalculateFinalPrice() {
 	order, err := entity.NewOrder("1", 10, 2)
 	suite.NoError(err)
 	order.CalculateFinalPrice()
@@ -43,7 +45,6 @@ func (suite *CalculatePriceUseCaseTestSuite) TestCalculatePriceFinalPrice() {
 		Price: order.Price,
 		Tax:   order.Tax,
 	}
-
 	calculateFinalPriceUseCase := NewCalculateFinalPriceUseCase(suite.OrderRepository)
 	output, err := calculateFinalPriceUseCase.Execute(calculateFinalPriceInput)
 	suite.NoError(err)
