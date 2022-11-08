08c0896c404794d0dcb072b7f4e7b7a23daae965
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-11-08 20:35:11 -0300

    added tests fixes

--- internal/order/infra/database/order_repository_test.go
+++ internal/order/infra/database/order_repository_test.go
@@ -2,10 +2,12 @@ package database
 
 import (
 	"database/sql"
+	"testing"
+
 	"github.com/Cerebrovinny/products_monitor/internal/order/entity"
 	"github.com/stretchr/testify/suite"
-	"testing"
-	//sqlite3
+
+	// sqlite3
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -17,11 +19,11 @@ type OrderRepositoryTestSuite struct {
 func (suite *OrderRepositoryTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	suite.NoError(err)
-	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL PRIMARY KEY, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL)")
+	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
 	suite.Db = db
 }
 
-func (suite *OrderRepositoryTestSuite) TearDownSuite() {
+func (suite *OrderRepositoryTestSuite) TearDownTest() {
 	suite.Db.Close()
 }
 
@@ -29,15 +31,21 @@ func TestSuite(t *testing.T) {
 	suite.Run(t, new(OrderRepositoryTestSuite))
 }
 
-func (suite *OrderRepositoryTestSuite) TestGivenAnOrder_WhenISave_ThenIShouldSaveOrder() {
+func (suite *OrderRepositoryTestSuite) TestGivenAnOrder_WhenSave_ThenShouldSaveOrder() {
 	order, err := entity.NewOrder("123", 10.0, 2.0)
 	suite.NoError(err)
 	suite.NoError(order.CalculateFinalPrice())
-	repository := NewOrderRepository(suite.Db)
-	err = repository.Save(order)
+	repo := NewOrderRepository(suite.Db)
+	err = repo.Save(order)
 	suite.NoError(err)
 
-	//fill orderResult using the select and guarantee that the order is the same as the NewOrder
 	var orderResult entity.Order
-	err = suite.Db.QueryRow("SELECT id, price, tax, final_price FROM orders WHERE id = ?", order.ID).Scan(&orderResult.ID, &orderResult.Price, &orderResult.Tax, &orderResult.FinalPrice)
+	err = suite.Db.QueryRow("Select id, price, tax, final_price from orders where id = ?", order.ID).
+		Scan(&orderResult.ID, &orderResult.Price, &orderResult.Tax, &orderResult.FinalPrice)
+
+	suite.NoError(err)
+	suite.Equal(order.ID, orderResult.ID)
+	suite.Equal(order.Price, orderResult.Price)
+	suite.Equal(order.Tax, orderResult.Tax)
+	suite.Equal(order.FinalPrice, orderResult.FinalPrice)
 }
