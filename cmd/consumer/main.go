5d7b8a5de8203dffd57d4fc08c39aac4f51cde1a
Cerebrovinny <Cerebrovinny>
2022-11-26 12:22:05 -0300

    added http server

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Cerebrovinny/products_monitor/pkg/rabbitmq"
 	_ "github.com/mattn/go-sqlite3"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"net/http"
 	"time"
 )
 
@@ -28,14 +29,25 @@ func main() {
 	defer ch.Close()
 
 	out := make(chan amqp.Delivery)
-	forever := make(chan bool)
+
 	go rabbitmq.Consume(ch, out)
 
 	qtdWorkers := 5
 	for i := 0; i < qtdWorkers; i++ {
 		go worker(out, &uc, i)
 	}
-	<-forever
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: repository}
+		total, err := getTotalUC.Execute()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+
+	http.ListenAndServe(":8081", nil)
 }
 
 func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int) {
